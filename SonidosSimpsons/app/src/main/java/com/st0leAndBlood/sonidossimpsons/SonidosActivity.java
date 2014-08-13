ba8862553e364cf6cf9e574b35eb0b0c1f8530d9
AlfonsoMestres <alfonsomestres@gmail.com>
2014-08-14 00:25:08 +0200

    - implementada una primera version de mp

--- SonidosSimpsons/app/src/main/java/com/st0leAndBlood/sonidossimpsons/SonidosActivity.java
+++ SonidosSimpsons/app/src/main/java/com/st0leAndBlood/sonidossimpsons/SonidosActivity.java
@@ -2,53 +2,38 @@ package com.st0leAndBlood.sonidossimpsons;
 
 import android.app.Activity;
 import android.media.AudioManager;
-import android.media.SoundPool;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.IOException;
 
 
 public class SonidosActivity extends Activity {
 
     private int soundID;
     boolean loaded = false;
-    SoundPool soundPool;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sonidos);
+
         final Button button = (Button) findViewById(R.id.primerLboton);
-        setSoundPool();
+        mediaPlayer = MediaPlayer.create(this, R.raw.beep);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-                float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-                float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-                float volume = actualVolume / maxVolume;
-
-                if (loaded) {
-                    soundPool.stop(soundID);
-                    soundPool.play(soundID, volume, volume, 1, 0, 1f);
-
-                }
-
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                mediaPlayer.release();
+                mediaPlayer.start();
             }
         });
-    }
-
-    private void setSoundPool(){
-        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
-        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-            @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId,int status) {
-                loaded= true;
-            }
-        });
-
-        soundID = soundPool.load(this, R.raw.beep, 1);
     }
 
 }
